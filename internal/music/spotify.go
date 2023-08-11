049dab0e01b02cb1a18bae90bc9b75c670cbaa9b
ghaust <joseph.banzio@contentsquare.com>
2023-08-12 00:56:38 +0200

    feat(music): finalize searchNewReleases func

--- internal/music/spotify.go
+++ internal/music/spotify.go
@@ -2,16 +2,21 @@ package music
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
-
-	spotifyauth "github.com/zmb3/spotify/v2/auth"
+	"strconv"
+	"time"
 
 	"github.com/zmb3/spotify/v2"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/clientcredentials"
 )
+
+type SearchResponse struct {
+	Albums []spotify.SimpleAlbum
+	Tracks []spotify.FullTrack
+}
 
 func GetToken() (context.Context, *oauth2.Token, error) {
 	ctx := context.Background()
@@ -32,23 +37,27 @@ func InitSpotifyClient(ctx context.Context, token *oauth2.Token) *spotify.Client
 	return spotify.New(httpClient)
 }
 
-func SearchNewReleases(client *spotify.Client, ctx context.Context, artist string) {
-	results, err := client.Search(ctx, artist, spotify.SearchTypeTrack|spotify.SearchTypeAlbum)
+func SearchNewReleases(client *spotify.Client, ctx context.Context, artist string) SearchResponse {
+	var response SearchResponse
+	query := addFiltersToQuery(artist)
+	results, err := client.Search(ctx, query, spotify.SearchTypeTrack|spotify.SearchTypeAlbum|spotify.SearchTypeArtist)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if results.Albums != nil {
-		fmt.Println("Albums:")
-		for _, item := range results.Albums.Albums {
-			fmt.Println("   ", item.Name)
-		}
+	if results.Albums != nil && len(results.Albums.Albums) > 0 {
+		response.Albums = results.Albums.Albums
 	}
 
-	if results.Tracks != nil {
-		fmt.Println("Tracks:")
-		for _, item := range results.Tracks.Tracks {
-			fmt.Println("   ", item.Name, item.Album.Name, item.Artists, item.Album.ReleaseDate)
-		}
+	if results.Tracks != nil && len(results.Tracks.Tracks) > 0 {
+		response.Tracks = results.Tracks.Tracks
 	}
+
+	return response
+}
+
+func addFiltersToQuery(query string) string {
+	year, _, _ := time.Now().Date()
+	return query + "%tag:new%" + "year:" + strconv.Itoa(year)
 }
